4b6337564c029daf60f178e4e4c45927e6604193
yesfedor <yesfedor.go@gmail.com>
2021-03-04 16:56:26 +0400

    Hello

--- app/src/views/Top10.vue
+++ app/src/views/Top10.vue
@@ -4,6 +4,9 @@
       <div class="col-12 text-center">
         <h1 class="my-0 py-3">Top15</h1>
       </div>
+      <div class="col-12 text-center">
+        <h4 class="theme-text">Hello, my friend</h4>
+      </div>
     </div>
   </div>
 </template>
